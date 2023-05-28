33ca4258f94064bc9508c3bf65049e98142741ee
A.Wael <96313471+AbdelrahmanWael2@users.noreply.github.com>
2023-05-28 17:12:55 +0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        int[] arr = { 10, 4, 6, 7, 2, 1 };
-        MergeSort ms = new MergeSort(arr);
-        ms.printLast();
-        ms.printAll();
-        ms.getTime();
-    }
-}
