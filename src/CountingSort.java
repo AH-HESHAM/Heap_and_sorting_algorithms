24991cfb363f22aa710fad26c65ac1e2ca1f0b10
Mohamed-Eltobgy <mohamedhassan17602@yahoo.com>
2023-05-30 20:09:09 +0300

    update count

--- src/CountingSort.java
+++ src/CountingSort.java
@@ -11,10 +11,10 @@ public class CountingSort {
     private long time = 0;
 
     public CountingSort(int[] inputArr) {
-        long start = System.nanoTime();
         arr = inputArr;
         result = new int[arr.length];
         intermediate = new int[arr.length + 1][arr.length];
+        long start = System.nanoTime();
         min = findMin();
         time = (System.nanoTime() - start);
     }
@@ -63,63 +63,89 @@ public class CountingSort {
     public void sort() {
         long start = System.nanoTime();
         updateCount();
+        long unused;
         // start from end to preserve order of numbers having same value.
         for (int i = arr.length - 1; i >= 0; i--) {
             int element = arr[i];
             int index = countArray[element] - 1;
-            result[index] = element;
+            result[index] = element + min; // add the minimum value back to each element
             countArray[element]--;
+            unused = System.nanoTime();
+            intermediate[arr.length-i] = Arrays.copyOf(result, arr.length);
+            time -= (System.nanoTime() - unused);
         }
-        Arrays.fill(arr, 0);
-        for (int i = 0; i < arr.length; i++) {
-            intermediate[i] = Arrays.copyOf(arr, arr.length);
-            arr[i] = result[i] + min; // add the minimum value back to each element
-        }
-        intermediate[arr.length] = Arrays.copyOf(arr, arr.length);
         time += (System.nanoTime() - start);
+        //Arrays.fill(arr, 0);
+        for (int i = 0; i < arr.length; i++) {
+            //intermediate[i] = Arrays.copyOf(arr, arr.length);
+            arr[i] = result[i];
+        }
+
+        //intermediate[arr.length] = Arrays.copyOf(arr, arr.length);
+
     }
 
-    private void print1Array(int[] toPrint) {
-        System.out.print("[");
+    private void print1Array(int[] toPrint, FileWriter writer) throws IOException {
+        writer.append("[");
         for (int i = 0; i < toPrint.length - 1; i++) {
-            System.out.print(toPrint[i] + ", ");
+            writer.append(toPrint[i] + ", ");
         }
-        System.out.print(toPrint[toPrint.length - 1]);
-        System.out.print("]");
+        writer.append(toPrint[toPrint.length - 1] + "");
+        writer.append("]");
     }
 
     public void printLast() {
-        print1Array(arr);
-        System.out.println();
+        String path = "C:/Users/ADMIN/Desktop/DS 2 assignments/assignment 4/Heap_and_sorting_algorithms/src/countOut.txt";
+        try (FileWriter writer = new FileWriter(path)) {
+            print1Array(arr, writer);
+            writer.append("\n");
+            writer.append("Count sort time for size = " + arr.length + " in micro = " + ((time) / 1000) + "\n"
+                    + "Count sort time for size = " + arr.length + " in milli = " + ((time) / 1000000) + "\n");
+            //getTime("C:/Users/ADMIN/Desktop/DS 2 assignments/assignment 4/Heap_and_sorting_algorithms/src/countOut.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void printAll() {
-        for (int i = 1; i <= intermediate.length; i++) {
-            System.out.print(i + ") ");
-            print1Array(intermediate[i - 1]);
-            System.out.println();
+        String path = "C:/Users/ADMIN/Desktop/DS 2 assignments/assignment 4/Heap_and_sorting_algorithms/src/countOut.txt";
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.append("Intermediate:\n");
+            for (int i = 0; i < intermediate.length-1; i++) {
+                writer.append((i + 1) + ") ");
+                print1Array(intermediate[i], writer);
+                writer.append(", ");
+                writer.append("\n");
+            }
+            writer.append(intermediate.length + ") ");
+            print1Array(intermediate[intermediate.length-1], writer);
+            writer.append("\nCount sort time for size = " + arr.length + " in micro = " + ((time) / 1000) + "\n"
+                    + "Count sort time for size = " + arr.length + " in milli = " + ((time) / 1000000) + "\n");
+            //getTime("C:/Users/ADMIN/Desktop/DS 2 assignments/assignment 4/Heap_and_sorting_algorithms/src/countOut.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     public void getTime(String path) {
         try (FileWriter writer = new FileWriter(path, true)) {
-            writer.append("Count sort time for size = " + intermediate[intermediate.length - 1].length + "in micro = "
-                    + ((time) / 1000) + "\n");
-            writer.append("Count sort time for size = " + intermediate[intermediate.length - 1].length + " in milli = "
-                    + ((time) / 1000000) + "\n");
+            writer.append("Count sort time for size = " + intermediate.length + "in micro = " + ((time) / 1000) + "\n"
+            + "Count sort time for size = " + intermediate.length + " in milli = " + ((time) / 1000000) + "\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    // public static void main(String[] args) {
-    // int[] arr = { -7538, -6475, -5335, -2770, -1459, -1028, -439, 869, 2858, 9381
-    // };
-    // CountingSort test = new CountingSort(arr);
-    // test.sort();
-    // for (int i = 0; i < arr.length+1; i++) {
-    // System.out.println(Arrays.toString(test.intermediate[i]));
-    // }
-    // test.getTime();
-    // //System.out.print(Arrays.toString(test.arr));
-    // }
+     public static void main(String[] args) {
+         int[] arr = { -7538, -6475, -5335, -2770, -1459, -1028, -439, 869, 2858, 9381};
+         CountingSort test = new CountingSort(arr);
+         test.sort();
+         //for (int i = 0; i < arr.length+1; i++) {
+         //System.out.println(Arrays.toString(test.intermediate[i]));
+         //}
+        // test.getTime();
+         //System.out.print(Arrays.toString(test.arr));
+         //test.printLast();
+         test.printAll();
+     }
 }
