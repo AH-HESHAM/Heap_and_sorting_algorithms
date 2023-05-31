c9e29db22f51f03ff56e0294105203ad80a94097
Mohamed <mohamedraffeek2001@gmail.com>
2023-05-31 15:29:09 +0300

    Added merge sort worst case generator

--- src/NumbersGenerator.java
+++ src/NumbersGenerator.java
@@ -50,4 +50,52 @@ public class NumbersGenerator {
             e.printStackTrace();
         }
     }
+    /* ------------Merge Sort Worst Case Generator------------ */
+
+    static void join(int arr[], int left[], int right[],
+                    int l, int m, int r)
+    {
+        int i;
+        for (i = 0; i <= m - l; i++)
+            arr[i] = left[i];
+
+        for (int j = 0; j < r - m; j++)
+            arr[i + j] = right[j];
+    }
+
+    // Function to store alternate elements in left
+    // and right subarray
+    static void split(int arr[], int left[], int right[],
+                     int l, int m, int r)
+    {
+        for (int i = 0; i <= m - l; i++)
+            left[i] = arr[i * 2];
+
+        for (int i = 0; i < r - m; i++)
+            right[i] = arr[i * 2 + 1];
+    }
+
+    // Function to generate Worst Case of Merge Sort
+    static void generateWorstCase(int arr[], int l, int r)
+    {
+        if (l < r)
+        {
+            int m = l + (r - l) / 2;
+
+            // create two auxiliary arrays
+            int[] left = new int[m - l + 1];
+            int[] right = new int[r - m];
+
+            // Store alternate array elements in left
+            // and right subarray
+            split(arr, left, right, l, m, r);
+
+            // Recurse first and second halves
+            generateWorstCase(left, l, m);
+            generateWorstCase(right, m + 1, r);
+
+            // join left and right subarray
+            join(arr, left, right, l, m, r);
+        }
+    }
 }
